6031dbfc8f97b8efdf71f4faf507af8e9f0001f6
ZachMommaerts <zmommaerts@gmail.com>
2024-01-09 11:06:35 -0600

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Zach Mommaerts on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
